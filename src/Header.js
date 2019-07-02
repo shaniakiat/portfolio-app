c1bbeabcf6669d888df6c4500b175a12cdf6a902
shaniakiat <kiat.shania@gmail.com>
2019-07-01 22:51:50 -0400

    design changes

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Background from './img/bg1-01.png';
+import Background from './img/pink-bg.jpg';
 import './Header.css';
 
 const myStyles = {
@@ -15,7 +15,7 @@ class Header extends Component {
             <header style={myStyles}>
                 <h1>{this.props.title}</h1>
                 <p>Welcome to My Portfolio</p>
-                <a href='#button'>{this.props.button}</a>
+                {/* <a href='#button'>{this.props.button}</a> */}
             </header>
         );
     }
